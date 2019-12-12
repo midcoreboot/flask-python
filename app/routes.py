d6a9ba85800f273bcc08027976929511eb4341e1
Rasmus Kolmodin <rasmus.kolmodin@gmail.com>
2019-12-12 00:58:04 -0800

    change

--- app/routes.py
+++ app/routes.py
@@ -52,7 +52,9 @@ def preview():
     if form.validate_on_submit() and text1 is not None and text2 is not None:
         X1 = form.left1.data
         Y1 = form.top1.data
-        return redirect(url_for('output', id=id, fontsize=fontsize, text1=text1, text2=text2, X1=X1, Y1=Y1))
+        X2 = form.left2.data
+        Y2 = form.top2.data
+        return redirect(url_for('output', id=id, fontsize=fontsize, text1=text1, text2=text2, X1=X1, Y1=Y1, X2=X2, Y2=Y2))
     return render_template('preview.html', imgpath=imgpath, fontsize=fontsize, text1=text1, text2=text2, form=form)
 
 
@@ -63,14 +65,28 @@ def output():
     fontsize = request.args['fontsize']
     text1 = request.args['text1']
     text2 = request.args['text2']
-    X1 = request.args['X1']
-    Y1 = request.args['Y1']
+    X1 = int(request.args['X1'])
+    Y1 = int(request.args['Y1'])
+    X2 = int(request.args['X2'])
+    Y2 = int(request.args['Y2'])
     basedir = os.path.dirname(__file__)
     im = Image.open(os.path.join(basedir, 'static/images', "1.jpg"))
+    width, height = im.size
+    xConst = X1 / 700
+    yConst = Y1 / 700
+    xConst2 = X2 / 700
+    yConst2 = Y2 / 700
+    newX = xConst * width
+    newY = yConst * height
+    newX2 = xConst2 * width
+    newY2 = yConst2 * height
+    '''
+    img cont  = 700px
+    '''
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype(os.path.join(basedir, 'fonts', 'Roboto-Regular.ttf'), int(fontsize))
-    draw.text((int(X1),int(Y1)), text1, (0,0,0), font=font)
-    draw.text((700,500), text2, (0,0,0), font=font)
+    draw.text((int(newX),int(newY)), text1, (0,0,0), font=font)
+    draw.text((int(newX2),int(newY2)), text2, (0,0,0), font=font)
     path = os.path.join(basedir, 'static/memes', '{}.jpg'.format(uid))
     im.save(path)
     app.logger.debug('X1: %s', X1)
