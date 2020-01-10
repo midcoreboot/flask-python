9a54452705efe497053446f0ce3a560fd6c4b58f
midcoreboot <rasmus.kolmodin@gmail.com>
2020-01-09 22:48:00 -0800

    From Home 1

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import sys
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDrawW
 from flask import render_template, flash, redirect, url_for, request, send_file
 from app import app
 from app.forms import ChooseForm, PreviewForm
@@ -18,12 +18,19 @@ def index():
         useD = form.useD.data
         text2 = form.textD.data
         if form.field.data == '1':
-            return redirect(url_for('preview', filename="1.jpg", fontsize=fontsize, text1=text1, text2=text2, useD=useD))
+            if useD == "True":
+                return redirect(url_for('preview', filename="1.jpg", fontsize=fontsize, text1=text1, text2=text2, useD=useD))
+            elif useD == "False":
+                return redirect(url_for('preview', filename="1.jpg", fontsize=fontsize, text1=text1, useD=useD))
 
              # Skicka vidare användaren till lämplig sida.
         elif form.field.data == '2':
-            return redirect(url_for('preview', filename="2.jpg", fontsize=fontsize, text1=text1, text2=text2, useD=useD))
-
+            if useD == "True":
+                app.logger.debug('used=true')
+                return redirect(url_for('preview', filename="2.jpg", fontsize=fontsize, text1=text1, text2=text2, useD=useD))
+            elif useD == "False":
+                app.logger.debug('used=false')
+                return redirect(url_for('preview', filename="2.jpg", fontsize=fontsize, text1=text1, useD=useD))
         else:
             return 'error'
 
@@ -36,23 +43,29 @@ def preview():
     imgpath = 'images/' + choice
     fontsize = request.args['fontsize']
     text1 = request.args['text1']
-    text2 = request.args['text2']
     useD = request.args['useD']
+    if useD == "True":
+        text2 = request.args['text2']
+
     form = PreviewForm()
     if form.validate_on_submit():
-        if useD == True and text1 is not None and text2 is not None:
+        if useD == "True" and text1 is not None and text2 is not None:
             X1 = form.left1.data
             Y1 = form.top1.data
             X2 = form.left2.data
             Y2 = form.top2.data
             return redirect(url_for('output', choice=choice, fontsize=fontsize, text1=text1, text2=text2, X1=X1, Y1=Y1, X2=X2, Y2=Y2, useD=useD))
-        elif useD == False and text1 is not None:
+        elif useD == "False" and text1 is not None:
             form.left2(disabled=True)
             form.top2(disabled=True)
             X1 = form.left1.data
             Y1 = form.top1.data
             return redirect(url_for('output', choice=choice, fontsize=fontsize, text1=text1, X1=X1, Y1=Y1, useD=useD))
-    return render_template('preview.html', imgpath=imgpath, fontsize=fontsize, text1=text1, text2=text2, form=form)
+
+    if useD == "True":
+        return render_template('preview.html', imgpath=imgpath, fontsize=fontsize, text1=text1, text2=text2, form=form)
+    elif useD == "False":
+        return render_template('preview.html', imgpath=imgpath, fontsize=fontsize, text1=text1, form=form)
 
 
 
@@ -68,7 +81,7 @@ def output():
     useD = request.args['useD']
 
     basedir = os.path.dirname(__file__)
-    im = Image.open(os.path.join(basedir, 'static/images', "choice"))
+    im = Image.open(os.path.join(basedir, 'static/images', choice))
     width, height = im.size
     xConst = width / 700
     yConst = height / 700
@@ -80,7 +93,7 @@ def output():
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype(os.path.join(basedir, 'fonts', 'Roboto-Regular.ttf'), int(fontsize))
     draw.text((int(newX),int(newY)), text1, (0,0,0), font=font)
-    if useD == True:
+    if useD == "True":
         X2 = int(request.args['X2'])
         Y2 = int(request.args['Y2'])
         newX2 = xConst * X2
